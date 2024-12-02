73a51ebee76799efc3b9bb79901c39dc8132efbf
chaubettom <chaubet.tom@aol.fr>
2024-12-02 17:38:31 +0100

    Re première version

--- make.R
+++ make.R
@@ -13,10 +13,10 @@
 devtools::install_deps(upgrade = "never")
 
 ## Load packages & functions ----
-devtools::load_all()ff
-
+devtools::load_all()
 
 # Run project ----
 
 ## Download raw data ----
 source(here::here("analyses", "download-data.R"))
+vdfv
